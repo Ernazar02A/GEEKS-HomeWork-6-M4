c51f7f074d53361574575866b449dfa11e3d066c
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-06-01 09:10:59 +0600

    Initial Commit

---
+++ GEEKS-HomeWork-6-M4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GEEKS-HomeWork-6-M4
+//
+//  Created by MacBook Pro on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
